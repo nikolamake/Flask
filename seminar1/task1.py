e054a9f602886a79ed83a3f26430d0b633afbdd0
nikolamake <112966899+nikolamake@users.noreply.github.com>
2023-08-30 21:06:46 +0300

    dz_sem1

---
+++ seminar1/task1.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/base/')
+def base():
+    return render_template('base.html')
+
+@app.route('/clothes/')
+def clothes():
+    return render_template('clothes.html')
+
+@app.route('/shoes/')
+def shoes():
+    return render_template('shoes.html')
+
+@app.route('/jackets/')
+def jackets():
+    return render_template('jackets.html')
+
+if __name__ == '__main__':
+    app.run()
+
+    
